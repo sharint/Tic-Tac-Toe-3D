2ee8aef8b3d035746f2461d095d92692474e4098
Tim <tim.kulizhnikov@mail.ru>
2021-10-15 23:00:13 +0700

    evening commit, game mechanics

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    private List<List<int>> winConditions;
+
+    public enum States { none, cross, circle };
+
+    private const int countSectors = 9;
+    private int turnsCount = 0;
+
+    public List<GameObject> sectors;
+
+    public List<int> freeSectors;
+    public List<int> playerSectors;
+    public List<int> enemySectors;
+
+
+    public GameObject sectorPrefab;
+
+    public Material cross;
+    public Material circle;
+
+    private void Start()
+    {
+
+        CreateBoard();
+        for(int i = 0; i < sectors.Count; i++)
+        {
+            freeSectors.Add(i);
+        }
+        
+    }
+
+    private void CreateBoard()
+    {
+        for(int i = 0; i < countSectors; i++)
+        {
+            Vector3 position = new Vector3(i % 3, i / 3, 0);
+            GameObject sector = Instantiate(sectorPrefab, position, sectorPrefab.transform.rotation);
+            sector.transform.SetParent(gameObject.transform);
+            sector.GetComponent<Sector>().id = i;
+            sectors.Add(sector);
+        }
+    }
+
+    public void Turn()
+    {
+        
+    }
+
+    private void EnemyTurn()
+    {
+        int randomID = Random.Range(0, freeSectors.Count);
+        int sectorIndex = freeSectors[randomID];
+        GameObject sector = sectors[sectorIndex];
+        sector.GetComponent<Sector>().state = Sector.States.cross;
+        SetStateSector(States.cross, sector);
+        freeSectors.Remove(sectorIndex);
+        enemySectors.Add(randomID);
+        turnsCount += 1;
+        if (IsGameOver())
+        {
+            print("Win");
+        }
+    }
+
+    private bool IsGameOver()
+    {
+        if (playerSectors.Count < 3 || enemySectors.Count < 3)
+        {
+            return false;
+        }
+        if(turnsCount >= 9)
+        {
+            return true;
+        }
+        for(int i = 1; i < 5; i++)
+        {
+            if (check(i))
+            {
+                //print("win");
+                return true;
+            }
+        }
+
+        return false;
+        
+    }
+
+    private bool check(int step)
+    {
+        int counter = 0;
+        for (int i = playerSectors[0]; i < 9; i += step)
+        {
+            if (playerSectors[counter] != i)
+            {
+                return false;
+            }
+            counter += 1;
+        }
+        return true;
+
+    }
+
+    public void PlayerTurn(int id)
+    {
+        GameObject sector = sectors[id];
+        SetStateSector(States.circle, sector);
+        freeSectors.Remove(id);
+        playerSectors.Add(id);
+        turnsCount += 1;
+        EnemyTurn();
+    }
+
+    private void SetStateSector(States state, GameObject sector)
+    {
+        switch (state) {
+            case States.circle:
+                sector.GetComponent<Renderer>().material = circle;
+                break;
+            case States.cross:
+                sector.GetComponent<Renderer>().material = cross;
+                break;
+            default:
+                print("Erorr, unknown state");
+                break;
+        }
+    }
+    
+}
---
+++ Assets/Scripts/Sector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sector : MonoBehaviour
+{
+    public enum States {none ,cross, circle };
+
+    public States state;
+
+    public int id;
+
+    private void Start()
+    {
+        state = States.none;
+    }
+
+    private void OnMouseDown()
+    {
+        if (state == States.none)
+        {
+            Board board = gameObject.GetComponentInParent<Board>();
+            board.PlayerTurn(id);
+            state = States.cross;
+        }
+        
+    }
+}
